1fba5ee37eb207bca4b9d1e0d4421a00283a5d38
Mihir-Gandhi <mihirgandhi03@gmail.com>
2016-12-05 20:00:39 +0530

    All Files

---
+++ app/js/services/bank.services.js
@@ -0,0 +1,42 @@
+﻿(function () {
+    'use strict';
+    angular
+        .module('ZybraBankApp')
+        .service("BankService", ['$http','$resource','$q', BankService]);
+
+    function BankService($http,$resource, $q) {
+        var ngAuthSettings = { apiServiceBaseUri: 'http://localhost:29926/' };
+        var resourceInstance = $resource(
+        ngAuthSettings.apiServiceBaseUri + "/BankApplication/:action",
+        {},
+        {
+            'getAllBanks': { method: 'GET', params: { action: 'GetBankData' } },
+            'getAllCityNames': { method: 'GET', params: { action: 'GetCityData' } },
+            'getAllBranches': { method: 'GET', params: { action: 'GetBranches' } }
+        });
+
+       this.getAllBanks = function () {
+           return resourceInstance.getAllBanks().$promise.then(function (response) {
+               if (response) {
+                   return response;
+               }
+           });
+       };
+
+       this.getAllCityNames = function (data) {
+           return resourceInstance.getAllCityNames(data).$promise.then(function (response) {
+               if (response) {
+                   return response;
+               }
+           });
+       };
+
+       this.getAllBranches = function (branchRequest) {
+           return resourceInstance.getAllBranches(branchRequest).$promise.then(function (response) {
+               if (response) {
+                   return response;
+               }
+           });
+       };
+    }
+})();
